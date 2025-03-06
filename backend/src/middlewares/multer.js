6b3d2dc5cf7a6828a98bd21770c882b1a3b66be0
16gsanket <16gsanket@gmail.com>
2025-03-06 11:03:03 +0530

    focefully creation of upload folder if not present

--- backend/src/middlewares/multer.js
+++ backend/src/middlewares/multer.js
@@ -1,6 +1,11 @@
 import multer from 'multer';
 import path from 'path';
 
+const uploadDir = './public/uploads';
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true }); // Create 'uploads' folder if missing
+}
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, './public/uploads'); // Save uploaded files to the 'uploads' folder
